e03fc22362c562c7e4e7d831eca8284f970f0400
doveka <131176206+doveka@users.noreply.github.com>
2023-12-27 23:50:07 +0200

    Add files via upload

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод значения времени
+        Console.WriteLine("Введите значение времени (в формате ЧЧ:ММ:СС):");
+        string input = Console.ReadLine();
+
+        // Проверка, является ли введенное значение временем
+        DateTime time;
+        bool isTime = DateTime.TryParse(input, out time);
+
+        if (isTime)
+        {
+            Console.WriteLine("Введенное значение является временем.");
+        }
+        else
+        {
+            Console.WriteLine("Введенное значение не является временем.");
+        }
+
+        // Ввод длительности работы посудомоечной машинки в минутах
+        Console.WriteLine("Введите длительность работы посудомоечной машинки (в минутах):");
+        int duration = int.Parse(Console.ReadLine());
+
+        // Ввод времени отсрочки начала запуска программы в часах
+        Console.WriteLine("Введите время отсрочки начала запуска программы (в часах):");
+        int delay = int.Parse(Console.ReadLine());
+
+        // Определение даты и времени начала мойки
+        DateTime startTime = DateTime.Now.AddHours(delay);
+
+        // Определение даты и времени окончания мойки
+        DateTime endTime = startTime.AddMinutes(duration);
+
+        // Вывод результатов
+        Console.WriteLine("Дата и время начала мойки: " + startTime);
+        Console.WriteLine("Дата и время окончания мойки: " + endTime);
+    }
+}
